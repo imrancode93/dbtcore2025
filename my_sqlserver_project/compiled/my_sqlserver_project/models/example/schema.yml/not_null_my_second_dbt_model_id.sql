e6729d0cf7a5488a03e4b2e9af1d7f95e44471da
imrancode93 <imranimmu280@gmail.com>
2025-02-07 13:10:31 +0530

    Add dbt docs for GitHub Pages

---
+++ my_sqlserver_project/compiled/my_sqlserver_project/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "AdventureWorks2014"."dbo"."my_second_dbt_model"
+where id is null
+
+
---
+++ my_sqlserver_project/target/compiled/my_sqlserver_project/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from "AdventureWorks2014"."dbo"."my_first_dbt_model"
+where id is null
+
+
